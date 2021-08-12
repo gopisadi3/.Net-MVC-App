7ae0e25fbcbebd0405e9fb8c3f359d63a51689b4
Gopi Sadi <170030401@kluniversity.in>
2021-08-12 14:40:09 +0530

    After Implementing Forms and valiation

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -49,9 +49,9 @@ namespace Vidly.Controllers
             if (movie == null)
                 return HttpNotFound();
 
-            var viewModel = new MovieFormViewModel
+            var viewModel = new MovieFormViewModel(movie)
             {
-                Movie = movie,
+                
                 Genres = _context.Genres.ToList()
             };
 
@@ -91,10 +91,21 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new MovieFormViewModel(movie)
+                {
+                    Genres = _context.Genres.ToList()
+                };
+
+                return View("MovieForm", viewModel);
+            }
             if (movie.Id == 0)
             {
+                
                 movie.DateAdded = DateTime.Now;
                 _context.Movies.Add(movie);
             }
